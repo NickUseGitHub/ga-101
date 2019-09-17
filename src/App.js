6c325d1fa53c41be2e57faaf27606cb46a561368
Nick <chaiwat@tencent.co.th>
2019-09-17 14:08:03 +0700

    [ga] create body

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import AppBody from './AppBody'
 
 function App() {
   return (
@@ -19,8 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <AppBody />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/AppBody.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function AppBody() {
+  return <div>Body Body Body</div>
+}
